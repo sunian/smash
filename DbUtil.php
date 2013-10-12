32ed75cddcd4b8aabe7b568ab647e23e0d8d5258
James Sun <jamessun92@gmail.com>
2013-10-12 14:03:34 -0400

    added db connect class and php version checker

---
+++ DbUtil.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sun
+ * Date: 10/12/13
+ * Time: 1:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DbUtil {
+    public static $host = "stardock.cs.virginia.edu";
+    public static $user = "cs4750jcs5sb";
+    public static $pass = "starkid";
+    public static $database = "cs4750jcs5sb";
+
+    public static function connect(){
+        $conn = new mysqli(DbUtil::$host, DbUtil::$user, DbUtil::$pass, DbUtil::$database);
+        if ($conn->connect_errno){
+            echo "db connection failed";
+            $conn->close();
+            exit();
+        }
+        return $conn;
+    }
+
+}
---
+++ phpversion.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sun
+ * Date: 10/12/13
+ * Time: 2:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+ phpinfo()
+?>
