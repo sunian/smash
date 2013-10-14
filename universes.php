a28d00f335d73e67f147e1285b72d7808f8fe555
James Sun <jamessun92@gmail.com>
2013-10-13 23:21:30 -0400

    testing

--- universes.php
+++ universes.php
@@ -7,8 +7,8 @@
  * Time: 3:28 PM
  * To change this template use File | Settings | File Templates.
  */
-require_once('DbUtil.php');
-$conn = DbUtil::connect();
+//require_once('DbUtil.php');
+//$conn = DbUtil::connect();
 
 $stmt = $conn->stmt_init();
 if ($stmt->prepare("select name from universe")){
