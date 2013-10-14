8fa7c7ea53b09ad20b16b80ddf9ad861a8fe7784
James Sun <jamessun92@gmail.com>
2013-10-14 13:14:14 -0400

    test

--- libs/browser.php
+++ libs/browser.php
@@ -6,4 +6,4 @@
  * Time: 12:57 PM
  * To change this template use File | Settings | File Templates.
  */
-echo json_encode(get_browser($_SERVER['HTTP_USER_AGENT'], false));
+echo $_SERVER['HTTP_USER_AGENT'];
