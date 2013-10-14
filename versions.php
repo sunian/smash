cc6b1033792811a45defd45ff7d89b88ee6c342e
James Sun <jamessun92@gmail.com>
2013-10-14 00:27:32 -0400

    fix

--- versions.php
+++ versions.php
@@ -1,26 +1,26 @@
-<div id="div_universes" style="display: none;"><?php
-    /**
-     * Created by JetBrains PhpStorm.
-     * User: Sun
-     * Date: 10/12/13
-     * Time: 3:28 PM
-     * To change this template use File | Settings | File Templates.
-     */
-    require_once('DbUtil.php');
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sun
+ * Date: 10/12/13
+ * Time: 3:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+require_once('DbUtil.php');
 
-    $stmt = $conn->stmt_init();
-    if ($stmt->prepare("select version_id,
-                concat(abbreviation, coalesce(concat(' ', version_number),'')) as name
-                from version
-                order by name")
-    ) {
-        $stmt->execute();
-        $stmt->bind_result($version_id, $name);
-        while ($stmt->fetch()) {
-            echo "<p><input type='checkbox' />",$name,"</p>";
-        }
+$stmt = $conn->stmt_init();
+if ($stmt->prepare("SELECT version_id,
+                concat(abbreviation, coalesce(concat(' ', version_number),'')) AS name
+                FROM version
+                ORDER BY name")
+) {
+    $stmt->execute();
+    $stmt->bind_result($version_id, $name);
+    while ($stmt->fetch()) {
+        echo "<p><input type='checkbox' />", $name, "</p>";
     }
-    $stmt->close();
-    ?></div>
+}
+$stmt->close();
+?>
 <!--<script type="text/javascript">-->
 <!--</script>-->
