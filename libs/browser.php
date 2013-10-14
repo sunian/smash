9e82bd8cf2b0ef665d4edd937a4a4a199900ded5
James Sun <jamessun92@gmail.com>
2013-10-14 13:22:33 -0400

    test

--- libs/browser.php
+++ libs/browser.php
@@ -6,4 +6,8 @@
  * Time: 12:57 PM
  * To change this template use File | Settings | File Templates.
  */
-echo $_SERVER['HTTP_USER_AGENT'];
+if (stripos($_SERVER['HTTP_USER_AGENT'], "MSIE", 0) === false) {
+    echo "you're safe </br>";
+} else {
+    echo "you're using IE </br>";
+}
