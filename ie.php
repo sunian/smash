c29a9c13e01aba1ef73a664a053e96431f519f77
James Sun <jamessun92@gmail.com>
2013-10-14 14:01:45 -0400

    test

--- ie.php
+++ ie.php
@@ -24,11 +24,13 @@
 <h3>Error description: User is currently using Internet Explorer.</h3>
 <?php
 if ($_GET["ignore"]){
+
     echo "You have been notified of this error and will not be warned again.";
 } else {
     echo "<input type='button' value='Do not show this in the future' onclick='ignoreWarnings();'/>";
 }
 ?>
-
+<br>
+<p>If you happen to be the Link main known as InternetExplorer, I mean no disrespect, but you sir have a terrible alias.</p>
 </body>
 </html>
