13e1ae55babf28538f78b5b2bb4cf0b6646f3d4c
James Sun <jamessun92@gmail.com>
2013-10-14 21:19:15 -0400

    create new character

--- character.php
+++ character.php
@@ -75,7 +75,7 @@ if (strlen($json_input) > 0) {
         <td id="newChar"></td>
     </tr>
 </table>
-<input type="button" value="Create" onclick="createChar();">
+<input type="button" value="Create New\nCharacter Identity" onclick="createChar();">
 <?php include('libs/universes.php'); ?>
 </body>
 </html>
