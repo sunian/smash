096107bfe9471a83aa70ed377f8f7f9e8e3c56f6
James Sun <jamessun92@gmail.com>
2013-10-15 02:24:41 -0400

    create new char

--- character.php
+++ character.php
@@ -97,14 +97,17 @@ if (strlen($json_input) > 0) {
 
 </table>
 <div id="fixedFooter" class="fixedFooter">
-    <table style="display: inline-block; float: left; bottom: 0px" class="white">
-        <tr>
-            <td><input id="newName" placeholder="New name"></td>
-            <td><input id="newNick" placeholder="New Nickname"></td>
-            <td id="newChar"></td>
-        </tr>
-    </table>
-    <input type="button" value="Create New&#x00A;Character" onclick="createChar();" style="display: inline-block; float: left; bottom: 0px">
+    <div style="position: relative;">
+        <table style="display: inline-block; bottom: 0px" class="white">
+            <tr>
+                <td><input id="newName" placeholder="New name"></td>
+                <td><input id="newNick" placeholder="New Nickname"></td>
+                <td id="newChar"></td>
+            </tr>
+        </table>
+        <input type="button" value="Create New&#x00A;Character" onclick="createChar();"
+               style="display: inline-block; bottom: 0px">
+    </div>
 </div>
 <?php include('libs/universes.php'); ?>
 </body>
