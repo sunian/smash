7c2857a1337f9afa2652578bb60b8037af59ee2d
James Sun <jamessun92@gmail.com>
2013-10-15 19:48:41 -0400

    layout

--- character.php
+++ character.php
@@ -84,7 +84,7 @@ if (strlen($json_input) > 0) {
 </head>
 <body>
 <div id="pageHeader" style="background-color: #F1F1F1; color: #404040; padding: 8px;">
-    <img src="images/title_logo.png" style="margin: 8px 0px">
+    <img src="images/title_logo.png" style="margin: 0px 8px">
     Hi, this is the nav header
 </div>
 <div class="body">
