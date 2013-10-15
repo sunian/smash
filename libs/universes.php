7ba63decedee942a5337c3d2b6efc3af2f662b99
James Sun <jamessun92@gmail.com>
2013-10-14 23:58:36 -0400

    switching from mysqli to PDO (so much better!!)

--- libs/universes.php
+++ libs/universes.php
@@ -11,16 +11,8 @@
     $conn = DbUtil::connect();
     $stmt = $conn->prepare("select universe_id, name from universe order by universe_id");
     $stmt->execute();
-//    $universes = array();
-//    $count = 0;
-    print_r($stmt->fetchAll());
-//    while ($row = $stmt->fetch()) {
-//        $universes[$count++] = array(
-//            "id" => $id,
-//            "name" => $name
-//        );
-//    }
-//    echo json_encode($universes);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    echo json_encode($stmt->fetchAll());
     $stmt->closeCursor();
     ?></div>
 <script type="text/javascript">
@@ -28,7 +20,7 @@
         var select_universe = document.createElement("select");
         var universes = JSON.parse($("#div_universes").text());
         for (var i in universes) {
-            select_universe.options[i] = new Option(universes[i].name, universes[i].id);
+            select_universe.options[i] = new Option(universes[i].name, universes[i].universe_id);
         }
         return select_universe;
     }
