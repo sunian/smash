3ee73ed8c87381ad8daeabf8f3d477dc585b5971
James Sun <jamessun92@gmail.com>
2013-10-15 16:53:44 -0400

    alternate fixed header

--- character.php
+++ character.php
@@ -96,7 +96,7 @@ if (strlen($json_input) > 0) {
             </tr>
         </table>
     </div>
-    <div style="overflow: scroll; max-height: 75%">
+    <div style="overflow: scroll; max-height: 80%">
         <table id="tableChars">
             <tr>
                 <th>Name</th>
