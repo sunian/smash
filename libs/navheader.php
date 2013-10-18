69bb26dada200131d3070dc76f8e837a173a4953
James Sun <jamessun92@gmail.com>
2013-10-18 03:05:54 -0400

    added link to tournaments.php

--- libs/navheader.php
+++ libs/navheader.php
@@ -10,5 +10,6 @@
 <div id="pageHeader" style="background-color: #F1F1F1; color: #404040; ">
     <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px"></a>
     <a href="character.php">Characters</a>
+    <a href="tournaments.php">Tournaments</a>
     Hi, this is the nav header
 </div>
