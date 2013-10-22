6eb6863d5b71c339615892c14c38fc7e0bf47452
James Sun <jamessun92@gmail.com>
2013-10-22 16:29:19 -0400

    more restyling

--- libs/navheader.php
+++ libs/navheader.php
@@ -11,7 +11,9 @@
 background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(19,23,50,.9)), color-stop(100%,rgba(5,6,13,0.5)));
 background: -webkit-linear-gradient(top,  rgba(19,23,50,.9) 0%,rgba(5,6,13,0.5) 100%);
 
-        color: #404040; ">
+        color: #404040;
+        max-height: 15%;
+         ">
     <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px"></a>
     <a href="character.php">Characters</a>
     <a href="tournaments.php">Tournaments</a>
