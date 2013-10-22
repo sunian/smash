adce346e97b7a215e59d1669974b35149236a316
James Sun <jamessun92@gmail.com>
2013-10-22 16:48:31 -0400

    more restyling

--- libs/navheader.php
+++ libs/navheader.php
@@ -10,11 +10,10 @@
 <div id="pageHeader" style="
 background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(19,23,50,.9)), color-stop(100%,rgba(5,6,13,0.5)));
 background: -webkit-linear-gradient(top,  rgba(19,23,50,.9) 0%,rgba(5,6,13,0.5) 100%);
-
-        color: white;
+vertical-align: middle;
          ">
     <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px; max-height: 11%"></a>
-    <a href="character.php">Characters</a>
-    <a href="tournaments.php">Tournaments</a>
+    <a class="naviLink" href="character.php">Characters</a>
+    <a class="naviLink" href="tournaments.php">Tournaments</a>
     Hi, this is the nav header
 </div>
