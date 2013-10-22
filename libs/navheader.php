0529159595d1c5b1035b32eba45236b6071a41b6
James Sun <jamessun92@gmail.com>
2013-10-22 16:34:20 -0400

    more restyling

--- libs/navheader.php
+++ libs/navheader.php
@@ -13,7 +13,7 @@ background: -webkit-linear-gradient(top,  rgba(19,23,50,.9) 0%,rgba(5,6,13,0.5) 
 
         color: #404040;
          ">
-    <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px; height: 13%"></a>
+    <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px; height: 10%"></a>
     <a href="character.php">Characters</a>
     <a href="tournaments.php">Tournaments</a>
     Hi, this is the nav header
