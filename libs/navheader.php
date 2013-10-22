e476e21560c7b03b9defdc0dbb7157bedc418e69
James Sun <jamessun92@gmail.com>
2013-10-22 19:42:25 -0400

    more restyling

--- libs/navheader.php
+++ libs/navheader.php
@@ -14,9 +14,9 @@
     <a href="index.php"><img src="images/title_logo.png" style="margin: 0px 8px;
         max-height: 11%;
         vertical-align: middle;"></a>
-    <ul style="list-style: none;">
-        <li><a class="naviLink clickable" href="character.php">Characters</a></li>
-        <li><a class="naviLink clickable" href="tournaments.php">Tournaments</a></li>
-    </ul>
+    <nav>
+        <a class="naviLink clickable" href="character.php">Characters</a>
+        <a class="naviLink clickable" href="tournaments.php">Tournaments</a>
+    </nav>
 
 </div>
