d8c50b9a9f9143f056c162441a25eca144bf9934
James Sun <jamessun92@gmail.com>
2013-10-22 22:59:01 -0400

    created blank pages

---
+++ players.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Sun
+ * Date: 10/18/13
+ * Time: 2:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+require_once('libs/browser.php');
+//require_once('libs/Character.php');
+if (strlen($json_input) > 0) {
+    exit();
+}
+?>
+
+<html>
+<head>
+    <title>Players</title>
+    <?php include('libs/headers.php'); ?>
+    <script type="text/javascript">
+    </script>
+</head>
+<body>
+<?php include('libs/navheader.php'); ?>
+
+</body>
+</html>
