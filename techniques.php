3ccb4080410905bfa29f064b25cb1203c34a7cd2
James Sun <jamessun92@gmail.com>
2013-10-29 18:03:17 -0400

    layout issues

--- techniques.php
+++ techniques.php
@@ -115,7 +115,7 @@ if (strlen($json_input) > 0) {
             $stmt->setFetchMode(PDO::FETCH_BOTH);
             while ($row = $stmt->fetch()) {
                 echo "<tr>";
-                echo "<td><a href='techniques.php?t=", $row["technique_id"], "'>", $row["abbreviation"], "</a></td>";
+                echo "<td>abbreviationabbreviation<a href='techniques.php?t=", $row["technique_id"], "'>", $row["abbreviation"], "</a></td>";
                 echo "<td>", $row["abbreviation"], "</td>";
                 echo "<td>", $row["abbreviation"], "</td>";
                 echo "</tr>";
