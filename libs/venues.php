6ae174fe1947521e91e8b293e7d994a9f32f3ba2
James Sun <jamessun92@gmail.com>
2013-10-29 22:20:13 -0400

    work on tournaments.php

--- libs/venues.php
+++ libs/venues.php
@@ -11,13 +11,16 @@
     $conn = DbUtil::connect();
     $stmt = $conn->prepare("select distinct venue from tournament order by venue");
     $stmt->execute();
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->setFetchMode(PDO::FETCH_NAMED);
     echo json_encode($stmt->fetchAll());
     $stmt->closeCursor();
     ?></div>
 <script type="text/javascript">
     function getVenues() {
         var venues = JSON.parse($("#div_venues").text());
+        for (var i in venues) {
+        }
+        venues = ["Link", "Sheik", "Marth", "Ganon", "DK", "Ike", "Pika"];
         return venues;
     }
 </script>
