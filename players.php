f3ddc46e5c814ac35aae277334d62ccb8970f02b
James Sun <jamessun92@gmail.com>
2013-10-29 23:19:20 -0400

    work on tournaments.php

--- players.php
+++ players.php
@@ -19,12 +19,58 @@ if (strlen($json_input) > 0) {
     <title>Players</title>
     <?php include('libs/headers.php'); ?>
     <script type="text/javascript">
+        var newDate, newVenue;
+        var newName, newRegion;
+        var btnAdd;
+        $(function () {
+            newName = $("#newName");
+            newDate = $("#newDate");
+            newVenue = $("#newVenue");
+            newRegion = $("#newRegion");
+            newName.keyup(function () {
+                btnAdd.css("display", newName.val().length > 0 ? "inline-block" : "none")
+            });
+            newVenue.autocomplete({
+                source: getVenues()
+            });
+
+            setupTables("Players");
+
+            newName.focus();
+        });
+
+        function createPlayers() {
+            var newTourny = {};
+            newTourny.name = newName.val();
+            newTourny.venue = $("#newVenue").val();
+            if (newTourny.name.length == 0) {
+                alert("Please enter a name to create a new tournament.");
+                newName.focus();
+                return;
+            }
+            if (newTourny.venue.length == 0) newTourny.venue = undefined;
+            newTourny.date = newDate.val();
+            newTourny.region = 0;
+            $.ajax({
+                type: "POST",
+                data: JSON.stringify(newTourny),
+                success: function (data, textStatus, jqXHR) {
+                    console.log(data);
+                    if (data.length > 0) {
+                        alert(data);
+                    } else {
+                        location.reload();
+                    }
+                }
+
+            });
+        }
     </script>
 </head>
 <body>
 <?php
 include('libs/navheader.php');
-$table = new DataTable("Tournaments", array(
+$table = new DataTable("Players", array(
     new TableColumn("Name", "newName", "input", "New name"),
     new TableColumn("Venue", "newVenue", "input", "New venue"),
     new TableColumn("Date", "newDate", "input", "New date")
