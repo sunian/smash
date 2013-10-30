7f976b5382ae10aa7c6ea83ceed400c6ef6dd9cc
James Sun <jamessun92@gmail.com>
2013-10-29 23:41:37 -0400

    work on tournaments.php

--- libs/navheader.php
+++ libs/navheader.php
@@ -21,6 +21,7 @@ if ($activePage == null) {
 <div id="pageHeader" style="
     background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(19,23,50,.9)), color-stop(100%,rgba(5,6,13,0.5)));
     background: -webkit-linear-gradient(top,  rgba(19,23,50,.9) 0%,rgba(5,6,13,0.5) 100%);
+    text-align: left;
          ">
     <a href="index.php"><img src="images/title_logo.png" style="margin: 0 8px;
         max-height: 11%;
