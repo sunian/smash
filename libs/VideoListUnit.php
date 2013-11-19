22dd01560ac0733b6b9bb95de0d23826c2e30283
Arjun Shankar <sarjun93@gmail.com>
2013-11-19 16:25:02 -0500

    VideoListUnit now populates its players field. Changed the display div to include a table

--- libs/VideoListUnit.php
+++ libs/VideoListUnit.php
@@ -14,8 +14,10 @@ class VideoListUnit {
     var $date_added;
     var $players;
     var $characters;
+    var $version;
 
     function __construct($video_id) {
+        // Get information from video table
         $conn = DbUtil::connect();
         $this->video_id = $video_id;
         $queryString = "SELECT title, url, date_added FROM video WHERE video_id = :video_id";
@@ -27,6 +29,16 @@ class VideoListUnit {
         $this->title = $result["title"];
         $this->url = $result["url"];
         $this->date_added = $result["date_added"];
+
+        // Get the players
+        $queryString = "SELECT tag FROM video NATURAL JOIN video_player NATURAL JOIN player WHERE video_id = :video_id";
+        $stmt = $conn->prepare($queryString);
+        $stmt->execute($params);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $counter = 0;
+        while($row = $stmt->fetch()) {
+            $this->players[$counter++] = $row["tag"];
+        }
     }
 
     public function getDisplayString() {
@@ -34,6 +46,12 @@ class VideoListUnit {
         $query = substr($query, strpos($query, "v=")+2);
         if(strpos($query, "&")>-1) $query = substr($query, 0, strpos($query, "&"));
         return "<div class='video_list_unit' id='". $this->video_id . "'>
-            <img src=\"http://img.youtube.com/vi/" . $query . "/1.jpg\"> </div>";
+            <table border="1">
+                <tr>
+                <td><img src=\"http://img.youtube.com/vi/" . $query . "/1.jpg\"></td>
+                <td>Players: </td>
+                </tr>
+            </table>
+            </div>";
     }
 }
