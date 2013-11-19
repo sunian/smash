f504e87a2c7f0a230c516159da85ba716ca5e754
Arjun Shankar <sarjun93@gmail.com>
2013-11-18 22:56:29 -0500

    Created VideoListUnit class for each video that will be displayed in a list on the website. As of now it just tries to show a thumbnail.

---
+++ libs/VideoListUnit.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Arjun
+ * Date: 11/18/13
+ * Time: 10:23 PM
+ */
+require_once('DbUtil.php');
+
+class VideoListUnit {
+    var $video_id;
+    var $title;
+    var $url;
+    var $date_added;
+    var $players;
+    var $characters;
+
+    function __construct($video_id) {
+        $conn = DbUtil::connect();
+        $this->video_id = $video_id;
+        $queryString = "SELECT title, url, date_added FROM video WHERE video_id = :video_id";
+        $params = array("video_id"=>$this->video_id);
+        $stmt = $conn->prepare($queryString);
+        $stmt->execute($params);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $stmt->fetchAll();
+        $this->title = $result["title"];
+        $this->url = $result["url"];
+        $this->date_added = $result["date_added"];
+    }
+
+    function __construct($video_id, $title, $url, $date_added) {
+//        $conn = DbUtil::connect();
+        $this->video_id = $video_id;
+        $this->url = $url;
+        $this->date_added = $date_added;
+        $this->title = $title;
+    }
+
+    public function echoDisplayString() {
+        echo "<div id='video_list_unit'> <img src=\"" , $this->url , "/1.jpg\"> </div>";
+    }
+}
