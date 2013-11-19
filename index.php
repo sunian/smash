402d2b558790ccdd9c0b24911e59625e3ee9b74c
Arjun Shankar <sarjun93@gmail.com>
2013-11-18 23:19:00 -0500

    Modified main page to use VideoListUnit

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
  */
 require_once('libs/browser.php');
 require_once('libs/DbUtil.php');
+require_once('libs/VideoListUnit.php');
 
 ?>
 
@@ -24,6 +25,15 @@ require_once('libs/DbUtil.php');
     </script>
 </head>
 <body>
-<?php include('libs/navheader.php'); ?>
+<?php include('libs/navheader.php');
+$conn = DbUtil::connect();
+$stmt = $conn->prepare("SELECT video_id FROM video ORDER BY date_added DESC");
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$x = 0;
+while($row = $stmt->fetch()) {
+    $listUnit = new VideoListUnit($row["video_id"]);
+    $listUnit->echoDisplayString();
+}
+?>
 </body>
 </html>
