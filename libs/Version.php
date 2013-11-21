7f024ff58fcadc890029a957da25a6b62f17ece2
Arjun Shankar <sarjun93@gmail.com>
2013-11-21 16:12:16 -0500

    Finished createIdentity() in Version.php

--- libs/Version.php
+++ libs/Version.php
@@ -7,26 +7,32 @@
  */
 
 require_once('DbUtil.php');
+require_once('JSONObject.php');
 
-class Version {
-    var $name;
-    var $release_date;
+class Version extends JSONObject{
+    var $title;
+    var $release_date = null;
+    var $version_number = null;
+    var $abbreviation = null;
 
     public function createIdentity() {
         try {
             $conn = DbUtil::connect();
-            $sql_string = "SELECT  FROM video WHERE url = :url";
-            $params = array("url" => $this->url);
+            $sql_string = "SELECT title FROM version WHERE title = :title" . $this->version_number?" AND version_number = :version_number":"";
+            $params = array("title"=>$this->title, "version_number"=>$this->version_number, "abbreviation"=>$this->abbreviation,
+                "release_date"=>$this->release_date);
             $stmt = $conn->prepare($sql_string);
             $stmt->execute($params);
             if ($row = $stmt->fetch()) {
                 // Error message!!
-                return "That video already exists!";
+                return "That version already exists!";
             }
             $stmt->closeCursor();
-            $sql_string = "INSERT INTO video (title, url, date_added) VALUES (:title, :url, NOW())";
+            $sql_string = "INSERT INTO version (title" . $this->release_date?", release_date":"" .
+                $this->version_number?", version_number":"" . $this->abbreviation?", abbreviation)":")";
+            $sql_string = $sql_string . "VALUES(:title" . $this->release_date?", :release_date":"" .
+                $this->version_number?", :version_number":"" . $this->abbreviation?", :abbreviation)":")";
             $stmt = $conn->prepare($sql_string);
-            $params["title"] = $this->title;
             $stmt->execute($params);
             $stmt->closeCursor();
             return false;
