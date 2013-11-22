3b34cf4a4118b523b3e118c19ab8e6dfeb0af693
ta3fh <ta3fh@virginia.edu>
2013-11-22 15:13:54 -0500

    Modified title

--- video.php
+++ video.php
@@ -1,8 +1,32 @@
 <?php
 /**
  * Created by JetBrains PhpStorm.
- * User: Student
- * Date: 11/21/13
- * Time: 4:10 PM
+ * User: Sun
+ * Date: 10/18/13
+ * Time: 2:53 AM
  * To change this template use File | Settings | File Templates.
  */
+require_once('libs/browser.php');
+require_once('libs/Videos.php');
+if (strlen($json_input) > 0) {
+   // $video = new Video($json_input);
+   // $error = $video->createIdentity();
+   // if ($error) echo $error;
+   // exit();
+}
+?>
+
+<html>
+<head>
+    <title>Video</title>
+    <?php include('libs/headers.php'); ?>
+    <script type="text/javascript">
+
+    </script>
+</head>
+<body>
+<?php include('libs/navheader.php');
+
+?>
+</body>
+</html>
