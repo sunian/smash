dc42bf1910b66b963de92bc98f031908e61b620a
Arjun Shankar <sarjun93@gmail.com>
2013-11-22 17:13:18 -0500

    version page uses Version

--- version.php
+++ version.php
@@ -6,6 +6,8 @@
  * Time: 3:15 PM
  */
 require_once('libs/browser.php');
+require_once('libs/Version.php');
+require_once('libs/Character.php');
 ?>
 <html>
 <head>
@@ -28,7 +30,10 @@ if(strcmp($urlParams["t"], "newVersion")==0) {
 
 }
 else {
-
+    $version = new Version();
+    $version->set(array("version_id"=>$urlParams["t"]));
+    $version->populateFieldsFromID();
+    echo "<h1>" , $version->pretty_name , "</h1>";
 }
 ?>
 </body>
