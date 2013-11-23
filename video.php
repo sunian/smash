528554b57b00c7828b8a8f122bd48144a0c63aac
ta3fh <ta3fh@virginia.edu>
2013-11-22 23:19:33 -0500

    Video page stuff

--- video.php
+++ video.php
@@ -46,26 +46,32 @@ require_once('libs/Videos.php');
     echo $vid->populateFieldsFromID();
     echo "<h1>", $vid->title , "</h1>";
     }
+
+    $urlID = $vid->getIDFromURL();
+    '<iframe width="425" height="350" type="application/x-shockwave-flash" ' .
+    'data="http://www.youtube.com/embed/' . $urlID . ' "?enable?enablejsapi=1&playsinline=1&autoplay=1">' .
+//  '<param name="video" value="http://www.youtube.com/' . $urlID . '"></param>' .
+     '</iframe>';
 ?>
-<script>
-function setURL() {
-    var iframe = document.getElementById("myframe");
-    var str1 = "http://www.youtube.com/embed/";
-    var str2 = vid->getIDFromURL();
-    var str3 = "?enable?enablejsapi=1&playsinline=1&autoplay=1";
-    var str = str1.concat(str2,str3);
-    iframe.src = str;
-}
-</script>
-<div class='body'>
-
-<iframe id="myframe"
-        width="420" height="345"
-        src="http://www.youtube.com/embed/aEd5doQuG9c?enablejsapi=1&playsinline=1&autoplay=1"
-        seamless
-        >
-</iframe>
-
-</div>
+<!--<script>-->
+<!--function setURL() {-->
+<!--    var iframe = document.getElementById("myframe");-->
+<!--    var str1 = "http://www.youtube.com/embed/";-->
+<!--    var str2 = vid->getIDFromURL();-->
+<!--    var str3 = "?enable?enablejsapi=1&playsinline=1&autoplay=1";-->
+<!--    var str = str1.concat(str2,str3);-->
+<!--    iframe.src = str;-->
+<!--}-->
+<!--</script>-->
+<!--<div class='body'>-->
+<!---->
+<!--<iframe id="myframe"-->
+<!--        width="420" height="345"-->
+<!--        src=""-->
+<!--        seamless-->
+<!--        >-->
+<!--</iframe>-->
+<!---->
+<!--</div>-->
 </body>
 </html>
