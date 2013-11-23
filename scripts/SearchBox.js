1bac72ccdc077dfa6316928d0d3b299f87daa06d
James Sun <jamessun92@gmail.com>
2013-11-22 23:04:27 -0500

    SearchBox.php

--- scripts/SearchBox.js
+++ scripts/SearchBox.js
@@ -1,7 +1,7 @@
 /**
  * Created by Sun on 10/30/13.
  */
-function setupSearchBox(){
+function setupSearchBox() {
     $("div.search-box").each(function (i, elem) {
         $(elem).html(new SearchBox($(elem).html()).render());
     });
@@ -10,8 +10,21 @@ function setupSearchBox(){
 function SearchBox(json) {
     var obj = JSON.parse(json);
     for (var prop in obj) this[prop] = obj[prop];
+    for (var i in this.fields) this.fields[i] = new QueryField(this.fields[i]);
 
     this.render = function () {
-        return this.title;
+        var output = this.title;
+        for (var i in this.fields) {
+            output += this.fields[i].render();
+        }
+        return output;
+    };
+}
+
+function QueryField(obj) {
+    for (var prop in obj) this[prop] = obj[prop];
+
+    this.render = function () {
+        return "<br><input id='" + this.id + "' placeholder='" + this.placeholder + "'/>";
     };
 }
