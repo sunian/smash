d71e815ccf95b0a4754c93e5d1f96fb833903ab7
Arjun Shankar <sarjun93@gmail.com>
2013-11-23 16:12:54 -0500

    Made VideoListUnit use Video class

--- libs/VideoListUnit.php
+++ libs/VideoListUnit.php
@@ -6,46 +6,20 @@
  * Time: 10:23 PM
  */
 require_once('DbUtil.php');
+require_once('Videos.php');
 
 class VideoListUnit {
-    var $video_id;
-    var $title;
-    var $url;
-    var $date_added;
-    var $players;
-    var $characters;
-    var $version;
+    var $video;
 
     function __construct($video_id) {
-        // Get information from video table
-        $conn = DbUtil::connect();
-        $this->video_id = $video_id;
-        $queryString = "SELECT title, url, date_added FROM video WHERE video_id = :video_id";
-        $params = array("video_id"=>$this->video_id);
-        $stmt = $conn->prepare($queryString);
-        $stmt->execute($params);
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetch();
-        $this->title = $result["title"];
-        $this->url = $result["url"];
-        $this->date_added = $result["date_added"];
-
-        // Get the players
-        $queryString = "SELECT tag FROM video NATURAL JOIN video_player NATURAL JOIN player WHERE video_id = :video_id";
-        $stmt = $conn->prepare($queryString);
-        $stmt->execute($params);
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $counter = 0;
-        while($row = $stmt->fetch()) {
-            $this->players[$counter++] = $row["tag"];
-        }
+        $this->video = new Video();
+        $array = array("video_id"=>$video_id);
+        $this->video->set($array);
+        $this->video->populateFieldsFromID();
     }
 
     public function getThumbnail() {
-        $query = substr($this->url, strpos($this->url, "?"));
-        $query = substr($query, strpos($query, "v=")+2);
-        if(strpos($query, "&")>-1) $query = substr($query, 0, strpos($query, "&"));
-        return "<img src=\"http://img.youtube.com/vi/" . $query . "/1.jpg\">";
+        return "<img src=\"http://img.youtube.com/vi/" . $this->video->getIDFromURL() . "/1.jpg\">";
     }
 
     public function getVideoInformation() {
