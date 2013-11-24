749c6cbd0c6907c570a48496f8ba1d7744e95af6
ta3fh <ta3fh@virginia.edu>
2013-11-23 20:39:56 -0500

    technique selector

--- video.php
+++ video.php
@@ -67,17 +67,16 @@ require_once('libs/Techniques.php');
     echo $output2;
 
     echo "<h1>", $vid->title , "</h1>";
-    $table = new DataTable("Techniques", array(
-    new TableColumn("Technique", "newTechnique", "select", "New Technique"),
-    ));
-    $table->setData("SELECT name
-            FROM `technique` WHERE technique_id = " . $vid->video_id, null);
-    $table->renderData = function ($row) {
-    echo "<tr>";
-    echo "<td>" . $row["techniques"] . "</td>";
-    echo "</tr>";
-    };
-    $table->render();
+//    $table = new DataTable("Techniques", array(
+//    new TableColumn("Technique", "newTechnique", "select", "New Technique"),
+//    ));
+//
+//    $table->renderData = function ($row) {
+//    echo "<tr>";
+//    echo "<td>" . $row["techniques"] . "</td>";
+//    echo "</tr>";
+//    };
+//    $table->render();
 
 include('libs/techniques.php');
 ?>
