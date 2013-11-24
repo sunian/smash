4d47f36714744d6c8963512b438a249bbe91ffdc
James Sun <jamessun92@gmail.com>
2013-11-23 22:11:48 -0500

    SearchBox.js

--- libs/versions.php
+++ libs/versions.php
@@ -1,28 +1,31 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Sun
- * Date: 10/12/13
- * Time: 3:28 PM
- * To change this template use File | Settings | File Templates.
- */
-require_once('DbUtil.php');
+<div id="div_versions" style="display: none;"><?php
+    /**
+     * Created by JetBrains PhpStorm.
+     * User: Sun
+     * Date: 10/18/13
+     * Time: 2:55 AM
+     * To change this template use File | Settings | File Templates.
+     */
+    require_once('DbUtil.php');
 
-$conn = DbUtil::connect();
-$stmt = $conn->prepare("SELECT version_id as id,
+    $conn = DbUtil::connect();
+    $stmt = $conn->prepare("SELECT version_id as id,
                 concat(abbreviation, coalesce(concat(' ', version_number),'')) AS name
                 FROM version
                 ORDER BY name");
-$stmt->execute();
-while ($row = $stmt->fetch()) {
-    echo "<label><input type='checkbox' value='", $row["id"], "'/>", $row["name"], "</label></br>";
-}
-$stmt->closeCursor();
-if ($_GET['enclose'] != null) {
-//    echo "enclose";
-} else {
-//    echo "don't enclose";
-}
-?>
-<!--<script type="text/javascript">-->
-<!--</script>-->
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    echo json_encode($stmt->fetchAll());
+    $stmt->closeCursor();
+    ?></div>
+<script type="text/javascript">
+    function createVersionSelector(includeBlank) {
+        var select_version = document.createElement("select");
+        var versions = JSON.parse($("#div_versions").text());
+        if (includeBlank) select_character.options[0] = new Option("Character", -1);
+        for (var i in versions) {
+            select_version.options[select_version.options.length] = new Option(versions[i].name, versions[i].id);
+        }
+        return select_version;
+    }
+</script>
