ed08bd294414421a90a7a9150eeba3f71f5ebd39
ta3fh <ta3fh@virginia.edu>
2013-11-23 20:27:49 -0500

    technique selector

--- video.php
+++ video.php
@@ -9,6 +9,9 @@
 require_once('libs/browser.php');
 require_once('libs/DbUtil.php');
 require_once('libs/Videos.php');
+require_once('libs/DataTable.php');
+require_once('libs/Techniques.php');
+
 //if (strlen($json_input) > 0) {
    // $video = new Video($json_input);
    // $error = $video->createIdentity();
@@ -41,21 +44,33 @@ require_once('libs/Videos.php');
     $vid->set(array("video_id"=>$urlParams["t"]));
     echo $vid->populateFieldsFromID();
     $urlID = $vid->getIDFromURL();
-    $players = $vid->players;
+
     for($i=0; $i<count($vid->players); $i++) {
-    $output = $output . ", " . $vid->players[$i]->tag;
+        $output = $output . ", " . $vid->players[$i]->tag;
     }
     echo $output;
-    $characters = $vid->characters;
+
     for($i=0; $i<count($vid->characters); $i++) {
-    $output2 = $output2 . ", " . $vid->characters[$i]->name;
+        $output2 = $output2 . ", " . $vid->characters[$i]->name;
     }
     echo $output2;
-    $techniques = $vid->techniques;
+
     echo "<h1>", $vid->title , "</h1>";
+    $table = new DataTable("Techniques", array(
+    new TableColumn("Technique", "newTechnique", "select", "New Technique"),
+    ));
+    $table->setData("SELECT name
+            FROM `technique` WHERE technique_id = " . $vid->video_id, null);
+    $table->renderData = function ($row) {
+    echo "<tr>";
+    echo "<td>" . $row["techniques"] . "</td>";
+    echo "</tr>";
+    };
+    $table->render();
 
-
+include('libs/techniques.php');
 ?>
+
 <div class='body'>
 <table>
     <tr>
@@ -68,12 +83,8 @@ require_once('libs/Videos.php');
         </td>
         <td style="background-color:black;width:425px;height=350">
             <h2>Information</h2><br>
-            Characters<br>
-            Players<br>
-            Techniques<br>
         </td>
     </tr>
-
 </table>
 </div>
 </body>
