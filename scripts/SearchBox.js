eee5eb45daf41177c1fa6651ebc30c227692f4ea
James Sun <jamessun92@gmail.com>
2013-11-23 22:00:33 -0500

    SearchBox.js

--- scripts/SearchBox.js
+++ scripts/SearchBox.js
@@ -58,7 +58,7 @@ function QueryField(myParent, obj) {
         if (this.count != "1")
             this.myDiv.prepend(document.createElement('br'));
         this.values.push(null);
-        for (var i in types) {
+        for (var i = types.length - 1; i >= 0; i--) {
             var typeData = types[i].split(":");
             switch (typeData[0]) {
                 case "input":
