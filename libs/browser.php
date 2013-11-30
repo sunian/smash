40ce0c753e40b6825235d573ccb00e8185c1c87e
James Sun <jamessun92@gmail.com>
2013-11-30 01:49:09 -0500

    more debugging

--- libs/browser.php
+++ libs/browser.php
@@ -18,8 +18,10 @@ function clean($elem)
     if(!is_array($elem) && !is_object($elem))
         $elem = htmlentities($elem,ENT_QUOTES,"UTF-8");
     else
-        foreach ($elem as $key => $value)
+        foreach ($elem as $key => $value) {
             $elem[$key] = clean($value);
+            echo $key, "=>", $value, "\n";
+        }
     return $elem;
 }
 
