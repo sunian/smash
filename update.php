012272996f2b33689a9bb5ebaa3a2b58633c5fe0
James Sun <jamessun92@gmail.com>
2013-11-29 19:51:15 -0500

    food for thought

--- update.php
+++ update.php
@@ -7,3 +7,26 @@
  */
 
 echo shell_exec("git pull");
+?>
+<body style="font-family: sans-serif">
+So once I sell all my litecoins, I'll have a total of $2000.
+With that sum of money, I could buy:
+<table>
+<tr><td>A new computer 	  </td><td>  MS Surface	 </td><td> ~ $280</td></tr>
+<tr><td>A new tablet	  </td><td>  Nexus 7	</td><td>	    ~ $230</td></tr>
+<tr><td>A new phone	      </td><td>  Nexus 5	</td><td>	    ~ $350</td></tr>
+<tr><td>A smartwatch	  </td><td>  by Google?  </td><td>	< $200</td></tr>
+<tr><td>A HUD		      </td><td>  Google Glass</td><td>	< $600</td></tr>
+<tr><td>A new gaming mouse		       </td><td></td><td>     < $20</td></tr>
+<tr><td>A new 1TB external HD		   </td><td></td><td>     ~ $60</td></tr>
+<tr><td>A LeapMotion Controller		   </td><td></td><td>     ~ $80</td></tr>
+<tr><td>Some RWBY merchandise</td><td>	:D</td><td>	< $50</td></tr>
+</table>
+And still have more than $100 left over<br>
+<br>
+...or....<br>
+<br>
+I could buy:<br>
+An Apple product (Macbook Pro) $1999<br>
+And have exactly $1 left over :/<br>
+</body>
