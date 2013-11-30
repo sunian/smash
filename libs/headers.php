cc03ac36168c1be017317478125b5d9eff6d70b9
James Sun <jamessun92@gmail.com>
2013-11-30 16:53:05 -0500

    change more links to https

--- libs/headers.php
+++ libs/headers.php
@@ -12,11 +12,11 @@
 <meta name="viewport" content="user-scalable=false"/>
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 <link rel="icon" href="images/favicon.ico" type="image/x-icon">
-<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-<script type="text/javascript" src="http://datejs.googlecode.com/files/date.js"></script>
-<script type="text/javascript" src="http://momentjs.com/downloads/moment.min.js"></script>
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/eggplant/jquery-ui.min.css" rel="stylesheet"
+<script type="text/javascript" src="https://datejs.googlecode.com/files/date.js"></script>
+<script type="text/javascript" src="https://momentjs.com/downloads/moment.min.js"></script>
+<link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/themes/eggplant/jquery-ui.min.css" rel="stylesheet"
       type="text/css"/>
 <link href="styles/default.css" rel="stylesheet" type="text/css"/>
 <script type="text/javascript" src="scripts/Helper.js"></script>
