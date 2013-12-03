dbd5b87ac607004c0ed19efbb058dc451e990d0d
Arjun Shankar <sarjun93@gmail.com>
2013-12-02 23:49:48 -0500

    Testing table border

--- index.php
+++ index.php
@@ -30,27 +30,6 @@ require_once('libs/DataTable.php');
 
 try {
     $conn = DbUtil::connect();
-    $stmt = $conn->prepare("SELECT video_id FROM video ORDER BY date_added DESC LIMIT 0, 10");
-    $stmt->execute();
-    $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    echo "<div class='body'>";
-    echo "<table id='most_recent_vids'>";
-    while($row = clean($stmt->fetch())) {
-        $listUnit = Video::nu($row["video_id"]);
-        echo "<tr id='" , $row["video_id"] , "'>
-                <td><a href='video.php?t=", $row["video_id"], "'>", $listUnit->renderThumbnail() , "</a></td>
-                <td>" , $listUnit->render() , "</td>
-              </tr>";
-    }
-    echo "</table>";
-    echo "</div>";
-}
-catch(PDOException $e) {
-    echo $e->getMessage();
-}
-
-try {
-    $conn = DbUtil::connect();
     $stmt = $conn->prepare("SELECT t.name, t.date, t.venue, r.name AS rName FROM tournament AS t INNER JOIN region as r on t.region_id
         = r.region_id ORDER BY t.date DESC LIMIT 0,3");
     echo "<div class='sideBlock'><table><tr><th>Recent Tournaments</th></tr><tr><td class='sideBlock'>";
@@ -65,6 +44,27 @@ catch(PDOException $e) {
     echo $e->getMessage();
 }
 
+
+try {
+    $conn = DbUtil::connect();
+    $stmt = $conn->prepare("SELECT video_id FROM video ORDER BY date_added DESC LIMIT 0, 10");
+    $stmt->execute();
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    echo "<div class='body'>";
+    echo "<table id='most_recent_vids' border='0'>";
+    while($row = clean($stmt->fetch())) {
+        $listUnit = Video::nu($row["video_id"]);
+        echo "<tr id='" , $row["video_id"] , "'>
+                <td><a href='video.php?t=", $row["video_id"], "'>", $listUnit->renderThumbnail() , "</a></td>
+                <td>" , $listUnit->render() , "</td>
+              </tr>";
+    }
+    echo "</table>";
+    echo "</div>";
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
 ?>
 </body>
 </html>
