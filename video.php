3eeb60e4c05988145ba4d043e001d88f2f9a7a77
ta3fh <ta3fh@virginia.edu>
2013-12-03 04:22:49 -0500

    player selector button

--- video.php
+++ video.php
@@ -9,14 +9,32 @@
 require_once('libs/browser.php');
 require_once('libs/DbUtil.php');
 require_once('libs/Video.php');
-//require_once('libs/Techniques.php');
+//require_once('libs/Technique.php');
 
 //if (strlen($json_input) > 0) {
-// $video = new Video($json_input);
-// $error = $video->createIdentity();
-// if ($error) echo $error;
-// exit();
+//$video = Video::nu($urlParams["t"]);
+//$technique = new Technique($json_input);
+// $error = $technique->createTechnique();
+// if ($error){
+//     echo $error;
+//     exit();
+// }
+//$conn = DbUtil::connect();
+//$tech_id=$technique->technique_id;
+//$v_id=$video->video_id;
+//$vp_id1 = "select max(video_player_id) from video player where video_id=$v_id";
+//$vp_id2 = "select min(video_player_id) from video player where video_id=$v_id";
+//$stmt = $conn->prepare("INSERT INTO technique_usage VALUES($tech_id,$vp_id1),($tech_id,$vp_id2)");
+//$params = array("technique_id"=>$technique->technique_id);
+//if($technique->) $params["date"] = $version->release_date;
+//if($version->version_number) $params["version_number"] = $version->version_number;
+//if($version->abbreviation) $params["abbrev"] = $version->abbreviation;
+//$stmt->execute($params);
+//$row = $stmt->fetch(PDO::FETCH_ASSOC);
+//exit();
+//INSERT INTO `cs4750jcs5sb`.`technique_usage` (`technique_id`, `video_player_id`) VALUES ('1', '4'), ('1', '5');
 //}
+
 if (!$urlParams["t"]) {
     header("Location: https://plato.cs.virginia.edu/~jcs5sb/smash/videos.php");
     exit();
@@ -37,14 +55,19 @@ $video = Video::nu($urlParams["t"]);
             selectTechnique = createTechniqueSelector();
             selectTechnique.id = "selectTechnique";
             newTechnique.append(selectTechnique);
+
+            newPlayer = $"#newPlayer");
+            selectPlayer = createPlayerSelector();
+            selectPlayer.id = "selectPlayer";
+            newPlayer.append(selectPlayer);
+
             Helper.displayBtnAdd(true);
         }
-        function createnewTechnique() {
-            alert("create new technique");
-//            var newObj = {};
-//            newObj.technique = $("#newTechnique").val();
-//            if (newObj.technique < 0) newObj.technique = undefined;
-//            Helper.uploadObj(newObj);
+        function addNewTechnique() {
+            var newObj = {};
+            newObj.technique_id = $("#newTechnique").val();
+     //       newObj.video_player_id = $()
+            Helper.uploadObj(newObj);
         }
     </script>
 </head>
@@ -79,16 +102,24 @@ echo "<h1>$video->title</h1>";
     <table id='newTechnique'>
         <tr>
             <td style="width:350px">
-                Add Techniques
+                Add Technique
             </td>
         </tr>
     </table>
-    <a href='javascript:void(0);' class='btnPlus' onclick='createnewTechnique()'> </a>
+    <table id='newPlayer'>
+        <tr>
+            <td style = width:350px">
+                To Player
+            </td>
+        </tr>
+    </table>
+    <a href='javascript:void(0);' class='btnPlus' onclick='addNewTechnique()'> </a>
 </div>
 </div>
 <?php
 //echo "<div id='div_urlParam' style='display: none;'>", $urlParams["t"], "</div>";
 include('libs/techniques.php');
+include('libs/players.php');
 ?>
 </body>
 </html>
