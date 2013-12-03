660ec79a9592ac5edd907f9df4d67aba0125d10b
ta3fh <ta3fh@virginia.edu>
2013-12-03 08:08:34 -0500

    Video Player debugging. I broke it with a typo??

--- video.php
+++ video.php
@@ -105,7 +105,7 @@ echo "<div id=\"div_players\" style=\"display: none;\">";
     var player_id;
     var video_id;
     var vp;
-    player_id = $(\"#newPlayers\").val();
+    player_id = $(\"#newPlayer\").val();
     video_id = " . $urlParams["t"] . ";" ;
 echo"    alert(player_id);
     alert(video_id);";
