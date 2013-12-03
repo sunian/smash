4d53a9c15a84cd0f7e0b27424ff2f9164583f24f
James Sun <jamessun92@gmail.com>
2013-12-03 02:15:03 -0500

    signin

--- libs/User.php
+++ libs/User.php
@@ -73,7 +73,7 @@ class User extends JSONObject
             $password = crypt(clean($stmt->fetchColumn()), $this->password);
             $stmt->closeCursor();
             if (strcmp($password, $this->password) == 0) {
-                return substr(crypt($password . microtime() . $this->username, '$2a$02$KenCombo.RedLikeRoses.'), 25, 30);
+                return hash("sha256", $password . microtime() . $this->username);
             }
 
         } catch (PDOException $e) {
