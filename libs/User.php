6ef24e3028ff93ae438d24f8277891f602d243c0
James Sun <jamessun92@gmail.com>
2013-12-03 04:14:24 -0500

    get access token

--- libs/User.php
+++ libs/User.php
@@ -73,7 +73,7 @@ class User extends JSONObject
             $password = crypt(clean($stmt->fetchColumn()), $this->password);
             $stmt->closeCursor();
             if (strcmp($password, $this->password) == 0) {
-                $sql_string = "call getAccessToken(:username, @token);select @token as token;";
+                $sql_string = "call getAccessToken(:username, @token); select @token as token;";
                 $stmt = $conn->prepare($sql_string);
                 $stmt->execute($params);
                 $token = clean($stmt->fetchColumn());
