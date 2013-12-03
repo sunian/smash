9efa1f37a8d82a831e77e7f784b2a974d22d6dbd
Arjun Shankar <sarjun93@gmail.com>
2013-12-03 09:28:46 -0500

    Possible fix

--- version.php
+++ version.php
@@ -130,7 +130,7 @@ if (strlen($json_input) > 0) {
                 newAirRank = $("#newAirRank");
 
                 Helper.setupDataTable("Characters");
-                Helper.displayBtnAdd(true);
+                Helper.displayBtnAdd(false);
 
                 newName = $("#newName");
                 newName.focus();
