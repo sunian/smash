d8119645c03d4e14c37ee7893449374519dd1dff
James Sun <jamessun92@gmail.com>
2013-12-03 06:58:45 -0500

    sign in

--- libs/browser.php
+++ libs/browser.php
@@ -41,7 +41,6 @@ if (stripos($_SERVER['HTTP_USER_AGENT'], "MSIE", 0) === false) {
 $authenticatedUser = null;
 
 if (isset($cookies["user_name"]) && isset($cookies["user_token"])) {
-    print_r($cookies["user_name"]);
     $authenticatedUser = User::nu($cookies["user_name"]);
     $authenticatedUser = $authenticatedUser->authenticateWithToken($cookies["user_token"]);
 }
